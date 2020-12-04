7c712e9fa5d11d6a1b8b9b806a6ad55eed71115c
Fitz_Chivel <korchahin1989@gmail.com>
2020-12-04 16:00:14 +0300

    models and ManyToMany

--- mainBlog/views.py
+++ mainBlog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from .models import Post, Tag
 
 def posts_list(request):
-    n = ["oleg", "Masha", "Olya"]
-    return render(request, 'mainBlog/index.html', context= {'names': n})
+    posts = Post.objects.all()
+    return render(request, 'mainBlog/index.html', context= {'posts': posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'mainBlog/post_detail.html', context={'posts': post})
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'mainBlog/tags_list.html', context={'tags': tags})
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return  render(request, 'mainBlog/tag_detail.html', context={'tag': tag})
