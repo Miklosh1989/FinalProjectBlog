3f0685decfb99efc8d892f6d60f1f3c9dc697e9b
FitZ <korchahin1989@gmail.com>
2021-03-22 22:28:12 +0300

    secretKey2

--- blog/settings.py
+++ blog/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-from blog import secret_key
+from blog.secret_key import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
